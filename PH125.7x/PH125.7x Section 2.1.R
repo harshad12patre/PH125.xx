8af64e59626e0fbdcd844cad920c4434f447766b
Harshad Barapatre <harshadbarapatre@hotmail.com>
2020-05-19 00:55:23 +0530

    Uploading new section

---
+++ PH125.7x/PH125.7x Section 2.1.R
@@ -0,0 +1,14 @@
+library(HistData)
+data("GaltonFamilies")
+
+data <- GaltonFamilies %>%
+  filter(gender=="male") %>%
+  select(father,childHeight) %>%
+  rename(son = childHeight)
+
+lm(son ~ father, data = data)
+
+galton_heights <- data %>%
+  mutate(father_centered=father - mean(father))
+
+lm(son ~ father_centered, data = galton_heights)
