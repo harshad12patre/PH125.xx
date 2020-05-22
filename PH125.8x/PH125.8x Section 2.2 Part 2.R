a68d46332d52c6daa4905413a4ae1b0e51c6abe2
Harshad Barapatre <harshadbarapatre@hotmail.com>
2020-05-23 01:33:47 +0530

    Uploading Section 2.2

---
+++ PH125.8x/PH125.8x Section 2.2 Part 2.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(dslabs)
+suppressWarnings(set.seed(1, sample.kind = "Rounding"))
+disease <- sample(c(0,1), size=1e6, replace=TRUE, prob=c(0.98,0.02))
+test <- rep(NA, 1e6)
+test[disease==0] <- sample(c(0,1), size=sum(disease==0), replace=TRUE, prob=c(0.90, 0.10))
+test[disease==1] <- sample(c(0,1), size=sum(disease==1), replace=TRUE, prob=c(0.15, 0.85))
+
+mean(test==1)
+mean(test==0 & disease==1)
+mean(test[disease==1]==1)*(mean(disease==1)/mean(test==1))
+(mean(test[disease==1]==1)*(mean(disease==1)/mean(test==1)))/mean(disease==1)
