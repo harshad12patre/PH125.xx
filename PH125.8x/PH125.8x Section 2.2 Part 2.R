36d2e1e9b265751c6f167345c07e934571137515
Harshad Barapatre <harshadbarapatre@hotmail.com>
2020-05-23 01:42:37 +0530

    Update PH125.8x Section 2.2 Part 2.R

--- PH125.8x/PH125.8x Section 2.2 Part 2.R
+++ PH125.8x/PH125.8x Section 2.2 Part 2.R
@@ -1,12 +1,24 @@
-library(tidyverse)
 library(dslabs)
-suppressWarnings(set.seed(1, sample.kind = "Rounding"))
-disease <- sample(c(0,1), size=1e6, replace=TRUE, prob=c(0.98,0.02))
-test <- rep(NA, 1e6)
-test[disease==0] <- sample(c(0,1), size=sum(disease==0), replace=TRUE, prob=c(0.90, 0.10))
-test[disease==1] <- sample(c(0,1), size=sum(disease==1), replace=TRUE, prob=c(0.15, 0.85))
+data("heights")
 
-mean(test==1)
-mean(test==0 & disease==1)
-mean(test[disease==1]==1)*(mean(disease==1)/mean(test==1))
-(mean(test[disease==1]==1)*(mean(disease==1)/mean(test==1)))/mean(disease==1)
+#6,7
+
+ps <- seq(0, 1, 0.1)
+heights %>%
+  mutate(g = cut(height, quantile(height, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(p = mean(sex == "Male"), height = mean(height)) %>%
+  qplot(height, p, data =.)
+
+#8
+
+Sigma <- 9*matrix(c(1,0.5,0.5,1), 2, 2)
+dat <- MASS::mvrnorm(n = 10000, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+
+ps <- seq(0, 1, 0.1)
+dat %>% 
+  mutate(g = cut(x, quantile(x, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(y = mean(y), x = mean(x)) %>%
+  qplot(x, y, data =.)
