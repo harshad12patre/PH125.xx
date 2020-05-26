3f3c30c32f2a58cb2fe68de1b363c278fb5f18d5
Harshad Barapatre <harshadbarapatre@hotmail.com>
2020-05-27 01:36:08 +0530

    Uploading Sections 3.3 and 4.1

---
+++ PH125.8x/PH125.8x Section 3.3.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(dslabs)
+library(tidyr)
+
+data <- read_mnist()
+
+dat <- data$train
+
+dat <- dat[["images"]]
+
+dat1 <- as.matrix(dat[dat > 50 & dat < 205])
+
+
+elements_dat <- ncol(dat) * nrow(dat)
+elements_dat1 <- nrow(dat1)
+
+elements_dat1/elements_dat
